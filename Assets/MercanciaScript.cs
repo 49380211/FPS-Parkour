61c39e371ae98a01766f3073eea3899297db49fc
49380211 <49380211@est.ort.edu.ar>
2025-06-05 12:07:19 -0300

    fiumba

--- Assets/MercanciaScript.cs
+++ Assets/MercanciaScript.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class MercanciaScript : MonoBehaviour
 {
+    public float cost;
+    public MoneyManager moneyManager;
+
+    void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+
     // Start is called before the first frame update
     void OnCollisionEnter(Collision collision)
     {
        if (collision.gameObject.name == "Player")
        {
+          //actualizar dinero por la compra
+          moneyManager.UpdateMoney(-cost);
           Destroy(gameObject);
 
        }
